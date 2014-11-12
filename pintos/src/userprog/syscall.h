a73b0f87ed8ca45388f620398d6469515c67866f
Adam Liebl <aliebl@pilot.cis.ksu.edu>
2014-11-12 12:41:29 -0600

    switched over to pintos2 base

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -2,7 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
-static void copy_in(const char *dest, char *start, int size);
-static char *copy_in_string(const char *us);
+void syscall_exit (void);
 
 #endif /* userprog/syscall.h */
